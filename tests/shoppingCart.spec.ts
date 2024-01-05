948e083a5a06ddeb9e5d8bcc528f2724abbc814a
Anna Maletka <maletka.a@icloud.com>
2024-01-05 12:59:49 +0100

    test: add second test shoppingCart

--- tests/shoppingCart.spec.ts
+++ tests/shoppingCart.spec.ts
@@ -5,10 +5,14 @@ import { ShoppingCartPage } from "../page-objects/ShoppingCartPage"
 import { CheckoutInformationPage } from "../page-objects/CheckoutInformationPage"
 import { CheckoutOverviewPage } from "../page-objects/CheckoutOverview"
 import { CheckoutCompletePage } from "../page-objects/CheckoutCompletePage"
+import { getUserData } from "../builders/userBuilder"
+import { ProductDetailsPage } from "../page-objects/ProductDetailsPage"
 
 test.describe("Product sales process", () => {
   test(`When user buy product and fill form 
   then user receiver a message with purchase a product`, async ({ page }) => {
+    const user = getUserData()
+
     await loginToAccount(page, "standard_user")
     const dashboardPage = new DashboardPage(page)
     await expect(dashboardPage.iconShoppingCart).toBeEmpty()
@@ -18,11 +22,32 @@ test.describe("Product sales process", () => {
     const shoppingCartPage = new ShoppingCartPage(page)
     await shoppingCartPage.goToCheckout()
     const checkoutInformationPage = new CheckoutInformationPage(page)
-    await checkoutInformationPage.fillFormCheckoutStepOne()
+    await checkoutInformationPage.fillFormCheckoutStepOne(user)
     const checkoutOverviewPage = new CheckoutOverviewPage(page)
     await checkoutOverviewPage.clickButtonFinish()
     const checkoutCompletePage = new CheckoutCompletePage(page)
     await checkoutCompletePage.clickButtonBackHome()
     await expect(dashboardPage.iconShoppingCart).toBeEmpty()
+  })
+  test(`Given user can add product to cart 
+  then remove it and add this product again from product details page`, async ({
+    page,
+  }) => {
+    await loginToAccount(page, "standard_user")
+    const dashboardPage = new DashboardPage(page)
+    await expect(dashboardPage.iconShoppingCart).toBeEmpty()
+    await dashboardPage.addFirstProductToCart()
+    await dashboardPage.addSecondProductToCart()
+    await expect(dashboardPage.iconShoppingCart).toHaveText("2")
+    await dashboardPage.goToShoppingCart()
+    const checkoutOverviewPage = new CheckoutOverviewPage(page)
+    await checkoutOverviewPage.removeBikeProductInCart()
+    await expect(checkoutOverviewPage.shoppingCartOverview).toHaveText("1")
+    await checkoutOverviewPage.clickButtonContinueShopping()
+    await dashboardPage.goToDetailsBikeProduct()
+    const productDetailsPage = new ProductDetailsPage(page)
+    await expect(productDetailsPage.shoppingCartDetails).toHaveText("1")
+    await productDetailsPage.clickAddToCart()
+    await expect(productDetailsPage.shoppingCartDetails).toHaveText("2")
   })
 })
