160fa9fe1536d81368f53e6b4421ca59c78cafaa
Anna Maletka <maletka.a@icloud.com>
2024-01-06 13:24:36 +0100

    refactor: upadate po

--- page-objects/CheckoutInformationPage.ts
+++ page-objects/CheckoutInformationPage.ts
@@ -7,6 +7,7 @@ export class CheckoutInformationPage extends BasePage {
   public readonly inputLastnameCheckout: Locator
   public readonly inputpostalCodeCheckout: Locator
   public readonly buttonContinueCheckout: Locator
+  public readonly buttonCancel: Locator
 
   public constructor(page: Page) {
     super(page)
@@ -14,6 +15,7 @@ export class CheckoutInformationPage extends BasePage {
     this.inputLastnameCheckout = page.locator("#last-name")
     this.inputpostalCodeCheckout = page.locator("#postal-code")
     this.buttonContinueCheckout = page.locator("#continue")
+    this.buttonCancel = page.locator("#cancel")
   }
 
   public async fillFormCheckoutStepOne(user: User): Promise<void> {
@@ -21,5 +23,17 @@ export class CheckoutInformationPage extends BasePage {
     await this.inputLastnameCheckout.fill(user.lastname)
     await this.inputpostalCodeCheckout.fill(user.postelCode)
     await this.buttonContinueCheckout.click()
+  }
+
+  public async fillFormCheckoutStepOneWithoutClickButtonContinue(
+    user: User
+  ): Promise<void> {
+    await this.inputFirstNameCheckout.fill(user.firstname)
+    await this.inputLastnameCheckout.fill(user.lastname)
+    await this.inputpostalCodeCheckout.fill(user.postelCode)
+  }
+
+  public async clickButtonCancel(): Promise<void> {
+    await this.buttonCancel.click()
   }
 }
