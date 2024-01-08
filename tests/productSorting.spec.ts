9acf0aeaba944201805dfc4592c22455200ae1e4
Anna Maletka <maletka.a@icloud.com>
2024-01-08 12:27:42 +0100

    test: add test

--- tests/productSorting.spec.ts
+++ tests/productSorting.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "@playwright/test"
-import { checkPriceSort, loginToAccount } from "../support/helpers"
+import { checkPriceSort, checkNameSort, loginToAccount } from "../support/helpers"
 import { DashboardPage } from "../page-objects/DashboardPage"
 
 test.describe("Product order", () => {
@@ -8,15 +8,26 @@ test.describe("Product order", () => {
     const dashboardPage = new DashboardPage(page)
 
     const beforeSortPriceArray = await dashboardPage.getAllPrices()
-    console.log(beforeSortPriceArray)
     let isSorted = await checkPriceSort(beforeSortPriceArray)
     expect(isSorted).toBeFalsy()
 
     await dashboardPage.selectHowToSortProducts("lohi")
 
     const afterSortPriceArray = await dashboardPage.getAllPrices()
-    console.log(afterSortPriceArray)
     isSorted = await checkPriceSort(afterSortPriceArray)
+    expect(isSorted).toBeTruthy()
+  })
+
+  test(`When user sort product by name (Z to A),
+  then sees products sorted alphabetically from Z to A`, async ({ page }) => {
+    await loginToAccount(page, "standard_user")
+    const dashboardPage = new DashboardPage(page)
+    const beforeSortNameArray = await dashboardPage.getAllNames()
+    let isSorted = await checkNameSort(beforeSortNameArray)
+    expect(isSorted).toBeFalsy()
+    await dashboardPage.selectHowToSortProducts("za")
+    const afterSortNameArray = await dashboardPage.getAllNames()
+    isSorted = await checkNameSort(afterSortNameArray)
     expect(isSorted).toBeTruthy()
   })
 })
